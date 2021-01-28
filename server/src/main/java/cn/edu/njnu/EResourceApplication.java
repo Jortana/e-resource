65ee2cdf8241eb818e7d05b8660de8ee63c24297
Jortana <JylJensen@outlook.com>
2021-01-28 15:32:08 +0800

    first commit

---
+++ server/src/main/java/cn/edu/njnu/EResourceApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.njnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EResourceApplication.class, args);
+	}
+
+}
