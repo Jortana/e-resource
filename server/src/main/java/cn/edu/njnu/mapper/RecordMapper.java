7606cf6b94ec6f84a8d99cec40a0fea0c8f0d168
灶台无限大 <962289755@qq.com>
2021-08-13 12:33:18 +0800

    添加了根据用户浏览记录生成图谱的接口

--- server/src/main/java/cn/edu/njnu/mapper/RecordMapper.java
+++ server/src/main/java/cn/edu/njnu/mapper/RecordMapper.java
@@ -4,9 +4,14 @@ import cn.edu.njnu.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 @Mapper
 @Repository
 public interface RecordMapper {
     void addEntityRecord(int userID, String browseDate, String entityName);
     void addResourceRecord(int userID, String browseDate, int resourceID);
+
+    ArrayList<Map<String, Object>> record(int userID);
 }
